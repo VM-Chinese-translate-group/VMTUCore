2b88ccc9787f36f70e8020d3159156a58fd5cc2b
TexTrue <65154269+TexBlock@users.noreply.github.com>
2026-03-05 11:54:02 +0800

    add CNB mirror

--- src/main/java/top/vmctcn/vmtu/core/util/MirrorSources.java
+++ src/main/java/top/vmctcn/vmtu/core/util/MirrorSources.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 public enum MirrorSources {
     MAXING_CDN("http://cdn.maxing.site/"),
     GITEE("https://gitee.com/Wulian233/vmtu/raw/main/"),
+    CNB("https://cnb.cool/VMChineseTranslationGroup/VM-Resources/-/git/raw/main/"),
     GITHUB("https://raw.githubusercontent.com/VM-Chinese-translate-group/VM-Resources/refs/heads/main/");
 
     private final String mirrorUrl;
